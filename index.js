6e4c3ce235f0ce529293f58f9d4aad5257c1b6c6
andreiabal <bal.andreia@gmail.com>
2018-09-04 12:39:21 -0300

    indentação index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-function cardValidator(numberString) { 
-
-  if (typeof numberString !== "number") {
+function cardValidator(numberString) {
+	
+	if (typeof numberString !== "number") {
     throw new Error('Error: Please, enter only numbers.');
 	}
 
 	if (numberString.toString().length === 1) {
 		throw new Error('Error: Please, enter a quantity of digits that is valid.');  
-  }
-
-	/*const numberArray = [];
-	for (let i = 0; i < numberString.length; i++) {
-		let charAtPosition = numberString.charAt(i);
-		numberArray.push(parseInt (charAtPosition));
-	}*/
-
+	}
+	
 	const numberArray = [];
 	for (let i = 0; i < numberString.toString().length; i++) {
 		let charAtPosition = numberString.toString().charAt(i);
@@ -41,24 +35,3 @@ function cardValidator(numberString) {
 }
 
 module.exports = cardValidator;
-
-
-/* Dica do Rafa: 
-O erro está nessa parte
-for (let i = 0; i < numberString.length; i++) {
-	let charAtPosition = numberString.charAt(i);
-	numberArray.push(parseInt(charAtPosition));
-}
-
-// O numberString na verdade não é uma String
-// Então ele faz numberString.length e da erro
-// Você tem duas possibilidades, uma delas é
-// criar uma outra variavel tipo: 
-// const numberStringForReal = numberString.toString()
-// e usar ela aqui: 
-// numberString.length e numberString.charAt(i);
-// Ou fazer:
-// numberString.toString().length
-// e numberString.toString().charAt(i)
-// Obs: na sprint1 funcionou pq numberString realmente
-// era uma string, mas agora ela é um número inteiro*/
